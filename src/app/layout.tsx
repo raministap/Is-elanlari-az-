5bbd6f7303053847c13912812b44a2c129a61e3d
raministap <gozelov1992@gmail.com>
2026-01-30 01:59:00 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { AuthProvider } from '@/context/AuthContext'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import { AuthProvider } from '../context/AuthContext'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
